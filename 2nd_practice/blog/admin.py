380176be2ebe653f57e227ce99a932ddf5f28d84
wayhome25 <siwabada@gmail.com>
2017-06-16 12:56:29 +0900

    blog 앱 내 Post에 대한 댓글쓰기/수정/삭제 구현 - blog.models.Comment 모델생성 - blog.views.comment_new / comment_edit / comment_delete 뷰 구현

--- 2nd_practice/blog/admin.py
+++ 2nd_practice/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,3 +7,5 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'is_public', 'created_at']
     list_display_links = ['title']
     list_editable = ['is_public']
+
+admin.site.register(Comment)
--- 2nd_practice/blog/models.py
+++ 2nd_practice/blog/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -11,3 +13,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
+
+
+class Comment(models.Model):
+    # Post : Comment = 1 : N
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def get_edit_url(self):
+        return reverse('blog:comment_edit', args=[self.post.pk, self.pk])
+
+    def get_delete_url(self):
+        return reverse('blog:comment_delete', args=[self.post.pk, self.pk])
